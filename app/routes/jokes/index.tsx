0b50b9180abef56c78856bf30246e14732497a2d
Boris <boris.evetovic777@gmail.com>
2022-01-03 13:26:52 +0100

    Initial routes

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,8 @@
+export default function JokesIndexRoute() {
+  return (
+    <div>
+      <p>Heres a random joke:</p>
+      <p>I was wondering why te frisbee was getting bigger...</p>
+    </div>
+  );
+}
